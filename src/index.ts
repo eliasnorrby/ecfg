24e50ed1bb978b88511080e4fb0fa5da35c6a18b
Elias Norrby <elias.norrby@gmail.com>
2020-10-14 12:34:33 +0200

    feat: add learn more message

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { log } from '@eliasnorrby/log-util'
 
 export function run(): void {
   log.info('Hello! This is a work in progress. Check back soon.')
+  log.info('Learn more at: https://github.com/eliasnorrby/ecfg')
 }
 
 run()

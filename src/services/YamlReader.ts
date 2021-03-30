91bfc4d4d4a87a8cba24e44912cac0ef86e7bd32
Elias Norrby <elias.norrby@gmail.com>
2021-03-30 14:00:45 +0200

    feat: implement YamlReader

--- src/services/YamlReader.ts
+++ src/services/YamlReader.ts
@@ -1,12 +1,37 @@
+import { log } from '@eliasnorrby/log-util'
+import fs from 'fs'
+import yaml from 'js-yaml'
+import path from 'path'
+
 import {
   ConfigReader,
   ModuleContext,
   ModuleResourceConfigList,
 } from '@common/types'
 
+const BASE_PATH = path.join('./', 'src', 'modules')
+const CONFIG_FILE_NAME = 'config.yml'
+
 export class YamlReader implements ConfigReader {
   read(context: ModuleContext): ModuleResourceConfigList {
-    console.log(`reading ${context.name}`)
-    return {}
+    const configFile = this.configPath(context.name)
+    if (!fs.existsSync(configFile)) {
+      log.warn(
+        `Could not find config for module: ${context.name} at ${configFile}`
+      )
+      return {}
+    }
+    try {
+      const fileContents = fs.readFileSync(configFile, 'utf8')
+      const data = yaml.load(fileContents)
+      return data as ModuleResourceConfigList
+    } catch (error) {
+      log.warn(`Could not read config for module: ${context.name}`)
+      return {}
+    }
+  }
+
+  private configPath(module: string) {
+    return path.join(BASE_PATH, module, CONFIG_FILE_NAME)
   }
 }

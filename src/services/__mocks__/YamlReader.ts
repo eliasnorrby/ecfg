f21b01fb2ea2a0bdf7f9a1f7fea4251d6324267f
Elias Norrby <elias.norrby@gmail.com>
2021-03-30 12:34:06 +0200

    feat: add YamlReader

---
+++ src/services/__mocks__/YamlReader.ts
@@ -0,0 +1,44 @@
+import {
+  ConfigReader,
+  ModuleContext,
+  ModuleResourceConfigList,
+} from '@common/types'
+
+export const CONFIGS: { [Id: string]: ModuleResourceConfigList } = {
+  prettier: {
+    nodePackages: [
+      {
+        name: 'prettier',
+        isDev: true,
+      },
+      {
+        name: '@eliasnorrby/prettier-config',
+        skipOnNoInstall: true,
+      },
+    ],
+    files: [
+      {
+        name: 'prettier.config.js',
+        source: 'prettier.config.js',
+        noInstallFallback: 'prettier.default.config.js',
+      },
+    ],
+  },
+  git: {
+    files: [
+      {
+        name: '.gitignore',
+        source: 'gitignore',
+      },
+    ],
+  },
+}
+
+export class YamlReader implements ConfigReader {
+  read(context: ModuleContext): ModuleResourceConfigList {
+    if (!CONFIGS[context.name]) {
+      throw new Error(`Could not read config for module ${context.name}`)
+    }
+    return CONFIGS[context.name]
+  }
+}
